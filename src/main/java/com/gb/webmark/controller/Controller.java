1e7043afad04b6696a6386bca00ea92a5287b5d0
GBRODICICO <gbrodicico@straighterline.com>
2022-03-31 21:31:19 +0300

    init

--- src/main/java/com/gb/webmark/controller/Controller.java
+++ src/main/java/com/gb/webmark/controller/Controller.java
@@ -4,6 +4,7 @@ import com.gb.webmark.service.AddImageWatermark;
 import com.gb.webmark.service.AddTextWatermark;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,10 @@ public class Controller {
 
     private final AddTextWatermark addTextWatermark;
     private final AddImageWatermark addImageWatermark;
+    @GetMapping("/hello")
+    public String hello() throws IOException {
+        return "hello";
+    }
 
     @PostMapping(value = "mark-image", produces = MediaType.IMAGE_PNG_VALUE)
     public byte[] markImage(@RequestParam("original") MultipartFile original, @RequestParam("watermark") MultipartFile watermark,
